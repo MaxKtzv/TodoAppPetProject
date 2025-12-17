de7ba8ce290bab12eae12b8933fe74a7b417135c
MaxKtzv <93527739+MaxKtzv@users.noreply.github.com>
2025-12-17 04:52:06 +0300

    Add main and database dependencies

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+import models
+from database import SessionLocal, engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
